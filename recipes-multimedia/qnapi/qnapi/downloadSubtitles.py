1fa43e97c2912106ed7bc110595e0562cb011ec1
Bartosz Brzozowski <bartosz.brzozowski23@gmail.com>
2023-06-21 21:27:46 +0200

    merge pl subtitles to video file

--- recipes-multimedia/qnapi/qnapi/downloadSubtitles.py
+++ recipes-multimedia/qnapi/qnapi/downloadSubtitles.py
@@ -1,10 +1,108 @@
 import os
 import subprocess
+import json
+import shutil
 import sys
 
 class DownloadSubtitles():
     def __init__(self):
         self.qnapiConfigFile = "/etc/mediaserver/qnapi.ini"
+
+    def mergeSubtitlesLoop(self, videoDirectory):
+        filesList = os.listdir(videoDirectory)
+        for movie in filesList:
+            if (".mp4" in movie or ".mkv" in movie) and ".part" not in movie:
+                self.mergeSubtitlesToVideoFile(videoDirectory, movie)
+
+
+    def mergeSubtitlesToVideoFile(self, videoDirectory, movie):
+        ext = movie.split('.')[-1]
+        movieName = movie.replace(".%s"%ext,"")
+        print(movieName,"start")
+        movieTemp = "%s_temp.%s"%(movieName, ext)
+        movieFullPath = os.path.join(videoDirectory, movie)
+        movieTempFullPath = os.path.join(videoDirectory, movieTemp)
+        subtitlesName = "%s.pl.%s"%(movieName,"srt")
+        subtitlesNamePath = os.path.join(videoDirectory, subtitlesName)
+        availableSubtitles = self.getAvailableSubtitlesFromMovie(movieFullPath)
+        if "pl" in availableSubtitles:
+            print("video contains pl and eng subtitles")
+            return
+        if not os.path.exists(subtitlesNamePath):
+            print("subtitles doesn't exist: ", subtitlesNamePath)
+            return
+
+        preLen = len(availableSubtitles)
+        if ext == "mkv":
+            subtitleType = "ass"
+        elif ext == "mp4":
+            subtitleType = "mov_text"
+        else:
+            print("error with subtitle type")
+            return
+
+        if preLen == 0:
+            return
+        print(availableSubtitles)
+        if not self.addPlSubtitlesToMovie(subtitlesNamePath, movieFullPath, movieTempFullPath, preLen, subtitleType):
+            return
+        availableSubtitles = self.getAvailableSubtitlesFromMovie(movieTempFullPath)
+        postLen = len(availableSubtitles)
+        if postLen == preLen:
+            return
+
+        os.remove(movieFullPath)
+        shutil.copy2(movieTempFullPath, movieFullPath)
+        os.remove(movieTempFullPath)
+
+        print(availableSubtitles)
+        print(movieName,"succesfull")
+        print("-----------------------------")
+
+
+    def getAvailableSubtitlesFromMovie(self, movie):
+        process = subprocess.run(['ffprobe', '-of','json', '-show_entries', 'format:stream', movie], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+        #output, errors = process.communicate()
+        #print(errors.decode('utf-8'))
+        #print(output.decode('utf-8'))
+        result = json.loads(process.stdout)
+        subtitlesIndexesOfStream = []
+        if "streams" not in result:
+            print("error with ffprobe result")
+            return []
+
+        streams = result["streams"]
+        #print(streams)
+        for x in range(len(streams)):
+            codecName = streams[x]["codec_name"]
+            codecType = streams[x]["codec_type"]
+
+            #print(codecName)
+            #print(codecType)
+            if codecType == "subtitle":
+                subtitlesIndexesOfStream.append(x)
+
+        availableSubtitles = []
+        for index in subtitlesIndexesOfStream:
+            #print(streams[index])
+            #print("language:", streams[index]['tags']['language'])
+            if "language" in streams[index]['tags']:
+                availableSubtitles.append(streams[index]['tags']['language'])
+
+        return availableSubtitles
+
+    def addPlSubtitlesToMovie(self, subtitles, movie, movieTemp, indexOfNewSubtitle, subtitleType):
+        #ffmpeg -i Manifest\ S02E01\ Fasten\ Your\ Seatbelts.mkv -i Manifest\ S02E01\ Fasten\ Your\ Seatbelts.pl.srt -map 0 -map 1 -c copy -c:s:1 ass -metadata:s:s:1 language='pl' Manifest\ S02E01\ Fasten\ Your\ Seatbelts_pl.mkv
+        metadataArgument = "-metadata:s:s:%s"%(str(indexOfNewSubtitle))
+        ffmpeg_args = ['ffmpeg', '-y', '-i', movie, "-i", subtitles, "-map", "0", "-map", "1","-c", "copy","-c:s:1", subtitleType, metadataArgument, "language=pl", movieTemp]
+
+        process = subprocess.Popen(ffmpeg_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        process.wait()
+        #debug, error = process.communicate()
+        #print(debug)
+
+        return process.returncode == 0
 
     def downloadSubtitlesForTvShow(self, languages:list, dirPath):
         result = {}
@@ -25,8 +123,8 @@ class DownloadSubtitles():
                                 if movie not in result.keys():
                                     result[movie] = []
                                 result[movie].append(language)
-                    
-        return result            
+
+        return result
 
     def downloadSubtitles(self, languages:list):
         result = {}
@@ -44,7 +142,7 @@ class DownloadSubtitles():
                 if not movieDir in result:
                    result[movieDir] = []
                 if not self.subtitlesExist(filesInMovieDir, language):
-                    for movie in filesInMovieDir:                
+                    for movie in filesInMovieDir:
                         if (".mp4" in movie or ".mkv" in movie) and ".part" not in movie:
                             movieFullPath = os.path.join(movieDirFullPath, movie)
                             if self.qnapi(movieFullPath):
@@ -58,8 +156,8 @@ class DownloadSubtitles():
                 else:
                     print(language, "subtitle exists for movie",movieDir)
                     result[movieDir].append(language)
-                    
-        return result            
+
+        return result
 
     def lookingForSubtitles(self):
             result = {}
@@ -74,18 +172,18 @@ class DownloadSubtitles():
                 filesInMovieDir = os.listdir(movieDirFullPath)
                 if not movieDir in result:
                    result[movieDir] = []
-                for movie in filesInMovieDir:                
+                for movie in filesInMovieDir:
                    if ".srt" in movie:
                        result[movieDir].append(movie)
-                   
-            return result            
+
+            return result
 
 
     def configQNapi(self, language):
         file = open(self.qnapiConfigFile, "r")
         replacement = ""
         for line in file:
-            if "language=" in line:            
+            if "language=" in line:
                 line = "language=%s\n"%(language)
             replacement = replacement + line
 
@@ -93,7 +191,7 @@ class DownloadSubtitles():
         fout = open(self.qnapiConfigFile, "w")
         fout.write(replacement)
         fout.close()
-    
+
     def subtitlesExist(self, filesInDir, language):
         partName = ".%s.srt"%(language)
         for x in filesInDir:
@@ -122,7 +220,7 @@ class DownloadSubtitles():
         videoDir = ""
         # using the for loop
         for line in file:
-            if "media_dir=V" in line: 
+            if "media_dir=V" in line:
                 videoDir = line.split(',')[1].strip()
         file.close()
         return videoDir
@@ -130,16 +228,23 @@ class DownloadSubtitles():
 
 if __name__ == "__main__":
     download = DownloadSubtitles()
-    if len(sys.argv) == 2:
+
+    if len(sys.argv) == 3:
         pathForTvShows = sys.argv[1]
-        print("Download subtitles for:", pathForTvShows)
-        if os.path.isdir(pathForTvShows):
+        if not os.path.isdir(pathForTvShows):
+            print("wrong path")
+            exit()
+        type = sys.argv[2]
+        if type == "--download" or type=="-t":
+            print("Download subtitles for:", pathForTvShows)
             result = download.downloadSubtitlesForTvShow(["eng", "pl"], pathForTvShows)
             result = sorted(result.items())
             for key, value in result:
                 print(key, ": ", value)
-        else:
-            print("wrong path")
+        elif type == "--merge" or type == "-m":
+            print("merge subtitles in:", pathForTvShows)
+            download.mergeSubtitlesLoop(pathForTvShows)
+
     else:
         result = download.downloadSubtitles(["eng","pl"])
 
