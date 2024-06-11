93b75256ffec006aad6225886208a9a7a2c11f04
Bartosz Brzozowski <bartosz.brzozowski23@gmail.com>
2024-06-11 21:09:36 +0200

    update youtubedl-web

--- recipes-app/configscript/configscript/installScriptBase.sh
+++ recipes-app/configscript/configscript/installScriptBase.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 configure_other()
 {
     # if those serial is enable, blutooth not working
@@ -9,12 +8,6 @@ configure_other()
     systemctl enable mysqld.service
     systemctl disable dhcpcd.service
 	echo vm.swappiness=0 | tee -a /etc/sysctl.conf
-
-    # access for Apache to enable/disable alarm
-    chgrp www-data /etc/mediaserver/alarm.sh
-    chgrp www-data /lib/systemd/system/alarm.timer
-    chgrp www-data /etc/mediaserver/youtubedl.ini
-    chown 775 /etc/mediaserver/youtubedl.ini
 
     amixer sset "Master" 100%
 
@@ -34,46 +27,8 @@ configure_vim()
     fi
 }
 
-install_bootstrap_youtubedl()
-{
-    printf " ----- Bootstrap for YouTube configuration  ----- \n"
-    bootstrapExist=$(ls /opt/youtubedl-web/static/ | grep bootstrap | wc -l)
-    if [ $bootstrapExist -gt 0 ]; then
-        printf "Boostrap for youtubedl is installed \n"
-    else
-        cd /opt/youtubedl-web/static
-        wget https://github.com/twbs/bootstrap/releases/download/v5.0.0-beta1/bootstrap-5.0.0-beta1-dist.zip
-        unzip bootstrap-5.0.0-beta1-dist.zip
-        mv bootstrap-5.0.0-beta1-dist bootstrap-5.0.0 --force
-        rm bootstrap-5.0.0-beta1-dist.zip
-        # clock picker
-        wget https://github.com/weareoutman/clockpicker/archive/gh-pages.zip
-        unzip gh-pages.zip
-        mv clockpicker-gh-pages clockpicker
-        rm gh-pages.zip
-    fi
-}
-
-install_bootstrap_apache()
-{
-    printf " ----- Bootstrap for Apache configuration  ----- \n"
-    bootstrapExist=$(ls /usr/htdocs/ | grep bootstrap | wc -l)
-    if [ $bootstrapExist -gt 0 ]; then
-        printf "Boostrap for apache is installed \n"
-    else
-        cd /usr/htdocs
-        wget https://github.com/twbs/bootstrap/releases/download/v5.0.0-beta1/bootstrap-5.0.0-beta1-dist.zip
-        unzip bootstrap-5.0.0-beta1-dist.zip
-        mv bootstrap-5.0.0-beta1-dist bootstrap-5.0.0 --force
-        rm bootstrap-5.0.0-beta1-dist.zip
-    fi
-}
-
-
 set -e
 
-install_bootstrap_youtubedl
-install_bootstrap_apache
 configure_vim
 configure_other
 reboot
--- recipes-network/apache2/apache2_%.bbappend
+++ recipes-network/apache2/apache2_%.bbappend
@@ -1,12 +1,8 @@
 SUMMARY = "Apache config"
 FILESEXTRAPATHS:prepend := "${THISDIR}/${PN}:"
-SRC_URI += "file://youtubedl-web.conf \
-            file://apache2.service \
-"
+SRC_URI += "file://apache2.service"
 
 do_install:append () {
-	install -d ${D}/etc/apache2
-	install -m 0755 ${WORKDIR}/youtubedl-web.conf ${D}/etc/apache2/conf.d
 	sed -i 's~DocumentRoot "/usr/share/apache2/default-site/htdocs"~DocumentRoot "/usr/htdocs"~g' ${D}/${sysconfdir}/${BPN}/httpd.conf
 	sed -i 's~<Directory "/usr/share/apache2/default-site/htdocs"~<Directory "/usr/htdocs"~g' ${D}/${sysconfdir}/${BPN}/httpd.conf
 	sed -i 's~User daemon~User www-data~g' ${D}/${sysconfdir}/${BPN}/httpd.conf
@@ -16,5 +12,3 @@ do_install:append () {
 	sed -i 's~#LoadModule proxy_module /usr/libexec/apache2/modules/mod_proxy.so~LoadModule proxy_module /usr/libexec/apache2/modules/mod_proxy.so~g' ${D}/${sysconfdir}/${BPN}/httpd.conf
 	sed -i 's~#LoadModule proxy_http_module /usr/libexec/apache2/modules/mod_proxy_http.so~LoadModule proxy_http_module /usr/libexec/apache2/modules/mod_proxy_http.so~g' ${D}/${sysconfdir}/${BPN}/httpd.conf
 }
-
-FILES:${PN} += "/etc/apache2/conf.d/youtubedl.conf"
